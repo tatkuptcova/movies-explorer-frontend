da5edf02d1f72925dac63f56576b2a483ce381df
tatkuptcova <tatkuptcova@yandex.ru>
2021-12-07 21:09:26 +0100

    constant

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
+const beatfilmAPI = "https://api.nomoreparties.co/beatfilm-movies";
+const filmAPI = "";
+const internailError =
+  "Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз";
+
+export default { beatfilmAPI, filmAPI, internailError };
